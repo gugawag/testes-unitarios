65a92efa5959aa6a2b58651c3754d42e09e23f48
Gustavo Wagner <gugawag@gmail.com>
2022-06-10 10:45:03 -0300

    inicio

---
+++ src/main/java/modelo/Conta.java
@@ -0,0 +1,42 @@
+package modelo;
+
+public class Conta {
+
+    public Conta() {
+    }
+
+    public Conta(String numero, Double saldo, Usuario dono) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.dono = dono;
+    }
+
+    private String numero;
+    private Double saldo;
+    private Usuario dono;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Usuario getDono() {
+        return dono;
+    }
+
+    public void setDono(Usuario dono) {
+        this.dono = dono;
+    }
+
+}
